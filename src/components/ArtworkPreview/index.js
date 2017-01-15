4a7506e078e776961f2280e635923f6c2ff5e4d6
Mehran Mozaffari <mehran.mozaffari@gmail.com>
2017-01-15 21:37:42 +1100

    c

--- src/components/ArtworkPreview/index.js
+++ src/components/ArtworkPreview/index.js
@@ -5,47 +5,48 @@ import { Link } from "phenomic"
 import styles from "./index.css"
 
 export default class ArtworkPreview extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      isHovered: false
-    };
-  }  
-  render() {
-    const { __url, thumbnail, title, caption } = this.props;
-    //const pageDate = date ? new Date(date) : null
-    const theThumbnail = thumbnail
-    const theCaption = caption
-    return (
-      <div 
-        className={classNames([styles.imgwrapper], { [styles.hovered]: this.state.isHovered })}
-        onMouseOver={() => this.setState({isHovered: true})} 
-        onMouseOut={() => this.setState({isHovered: false})} 
-      >
-        <span className={styles.hover_text } >
-          
-            { theCaption.line1 } <br />
-          
+constructor (props) {
+super(props);
+this.state = {
+isHovered: false
+};
+}  
+render() {
+const { __url, thumbnail, title, caption } = this.props;
+//const pageDate = date ? new Date(date) : null
+const theThumbnail = thumbnail
+const theCaption = caption
+return (
+<div 
+className={classNames([styles.imgwrapper], { [styles.hovered]: this.state.isHovered })}
+onMouseOver={() => this.setState({isHovered: true})} 
+onMouseOut={() => this.setState({isHovered: false})} 
+>
+<span className={styles.hover_text } >
 
-          
-        </span>
+{ theCaption.line1 } <br />
 
-        <Link to={ __url } className={ styles.title }>
 
-            <img
-              src={theThumbnail.image}
-              className={ styles.imgt }
-            />
 
-        </Link>
-      </div>
-    );
-  }
+</span>
+
+<Link to={ __url } className={ styles.title }>
+
+<img
+src={theThumbnail.image}
+className={ styles.imgt }
+alt ={title}
+/>
+
+</Link>
+</div>
+);
+}
 }
 
 ArtworkPreview.propTypes = {
-  __url: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  caption: PropTypes.object.isRequired,
-  thumbnail: PropTypes.object.isRequired
+__url: PropTypes.string.isRequired,
+title: PropTypes.string.isRequired,
+caption: PropTypes.object.isRequired,
+thumbnail: PropTypes.object.isRequired
 }
--- src/components/AutoPlaySlider/index.js
+++ src/components/AutoPlaySlider/index.js
@@ -7,47 +7,47 @@ import styles from "./index.css"
 
 
 const AutoPlaySlider = ({ featuredArtworks }) => {
-  const settings = {
-   	  dots: false,
-   	  fade: true,
-   	  speed: 1500,
-   	  lazyLoad: true,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      nextArrow: false,
-      prevArrow: false
-    }
-  
-
-  return (
-      <div>
-        
-        
-        <Grid style={{width: '100%', margin: 'auto'}} >
-          
-            <Cell
-              col={ 12 }
-            >
-            <Slider {...settings}>
-            	{ featuredArtworks.map((featured) => (
-              		<div  key={ featured.__url  } ><img className={styles.imgaps} src={featured.featuredArtwork} /></div>
-              	)) }	
-			</Slider>
-            </Cell>
+const settings = {
+dots: false,
+fade: true,
+speed: 1500,
+lazyLoad: true,
+infinite: true,
+slidesToShow: 1,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 3000,
+nextArrow: false,
+prevArrow: false
+}
 
 
- 			
-        </Grid>
-        
-      </div>
-  )
+return (
+<div>
+
+
+<Grid style={{width: '100%', margin: 'auto'}} >
+
+<Cell
+col={ 12 }
+>
+<Slider {...settings}>
+{ featuredArtworks.map((featured) => (
+<div  key={ featured.__url  } ><img className={styles.imgaps} src={featured.featuredArtwork} /></div>
+)) }	
+</Slider>
+</Cell>
+
+
+
+</Grid>
+
+</div>
+)
 }
 
 AutoPlaySlider.propTypes = {
-  featuredArtworks: PropTypes.array.isRequired,
+featuredArtworks: PropTypes.array.isRequired,
 }
 
 export default AutoPlaySlider
